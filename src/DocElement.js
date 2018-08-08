d3e399822c6a9d30d99bcff1d9ff9fdcf9649643
TeeSeal <teesealz@gmail.com>
2018-08-08 14:13:11 +0300

    chore: refactor inheritance formatting for new docgen

--- src/DocElement.js
+++ src/DocElement.js
@@ -43,44 +43,20 @@ class DocElement extends DocBase {
     return this.doc.formatType(this.type)
   }
 
-  get formattedExtends () {
-    if(!this.extends) return '';
-    if(typeof this.extends[0] === 'string') {
+  get formattedInheritance () {
+    const [inherits, verb] = this.extends
+      ? [this.extends, 'extends']
+      : [this.implements, 'implements']
+
+    if (!inherits) return ''
+
+    const formatted = typeof inherits[0] === 'string'
       // docgen 0.8.0 format
-      const baseClasses = this.extends
-        .map(baseClass => (this.doc.get(baseClass) || { link: baseClass }).link)
-      return `(extends **${ baseClasses.join('** and **')}**)`
-    } else {
+      ? this.doc.formatType(inherits)
       // docgen 0.9.0 format
-      const baseClasses = this.extends
-        .map(baseClass => flatten(baseClass))
+      : inherits.map(baseClass => this.doc.formatType(flatten(baseClass))).join(' and ')
 
-      const formatted = baseClasses
-        .map(baseClass =>
-          baseClass.map(el => (this.doc.get(el) || { link: el }).link).join(''))
-
-      return `(extends **${ formatted.join('** and **')}**)`
-    }
-  }
-
-  get formattedImplements () {
-    if(!this.implements) return '';
-    if(typeof this.implements[0] === 'string') {
-      // docgen 0.8.0 format
-      const baseClasses = this.implements
-        .map(baseClass => (this.doc.get(baseClass) || { link: baseClass }).link)
-      return `(implements **${ baseClasses.join('** and **')}**)`
-    } else {
-      // docgen 0.9.0 format
-      const baseClasses = this.implements
-        .map(baseClass => flatten(baseClass))
-
-      const formatted = baseClasses
-        .map(baseClass =>
-          baseClass.map(el => (this.doc.get(el) || { link: el }).link).join(''))
-
-      return `(implements **${ formatted.join('** and **')}**)`
-    }
+    return `(${verb} ${formatted})`
   }
 
   get link () {
@@ -104,10 +80,7 @@ class DocElement extends DocBase {
     const embed = this.doc.baseEmbed()
     let name = `__**${this.link}**__`
 
-    if (this.extends) name += ' ' + this.formattedExtends;
-
-    if (this.implements) name += ' ' + this.formattedImplements;
-
+    if (this.formattedInheritance) name += ` ${this.formattedInheritance}`
     if (this.access === 'private') name += ' **PRIVATE**'
 
     embed.description = `${name}\n${this.formatText(this.description)}`
