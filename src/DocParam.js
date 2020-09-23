821b2e44c9a017f48df33b0a31bf043e4281a21f
NotSugden <28943913+NotSugden@users.noreply.github.com>
2020-09-23 16:17:22 +0100

    fix: correctly display rest params

--- src/DocParam.js
+++ src/DocParam.js
@@ -5,10 +5,16 @@ class DocParam extends DocElement {
     super(parent.doc, DocElement.types.PARAM, data, parent)
     this.type = data.type.flat(5)
     this.optional = data.optional
+    this.variable = data.variable
   }
 
   get formattedName () {
     return this.optional ? `\`[${this.name}]\`` : `\`${this.name}\``
+  }
+
+  get formattedType () {
+    if (!this.variable) return super.formattedType
+    return super.formattedType.split('|').map(param => `...${param}`).join('|')
   }
 
   get url () {
